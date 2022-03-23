8de85c3e597dab05c6e90e47d657f1f13e209058
Guilherme Dantas <guilherme.dantas1508@gmail.com>
2022-03-23 17:09:03 -0300

    Added tickets to homepage

--- dataHandler.js
+++ dataHandler.js
@@ -1,4 +1,7 @@
 var objRaw;
+var issues = [];
+let cardContainer;
+
 getJson()
 
 async function getJson() {
@@ -7,7 +10,53 @@ async function getJson() {
     objRaw = json;
 
     objRaw.issues.forEach(element => {
-        if(element.fields.hasOwnProperty('parent'))
-            console.log(element.fields)
+        if(element.fields.hasOwnProperty('parent')){
+            var elementFields = element.fields
+            var issue = new Issue(elementFields.summary, elementFields.description.content[0].content[0].text, elementFields.parent.fields.summary) 
+            issues.push(issue)
+        }
+            
     });
+
+    initListOfTasks();
 }
+
+let createTaskCard = (issue) => {
+    let card = document.createElement('div');
+    card.className = 'issue card shadow cursor-pointer';
+
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    let title = document.createElement('h5');
+    title.innerText = issue.title;
+    title.className = 'card-title';
+
+    let parent = document.createElement('div');
+    parent.innerText = issue.parent;
+    title.className = 'card-text';
+
+    let description = document.createElement('div');
+    description.innerText = issue.description;
+    description.className = 'card-text';
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(parent);
+    cardBody.appendChild(description);
+    card.appendChild(cardBody);
+    cardContainer.appendChild(card);
+}
+
+let initListOfTasks = () => {
+    if (cardContainer) {
+        document.getElementById('card-container').replaceWith(cardContainer);
+        return;
+    }
+
+    cardContainer = document.getElementById('card-container');
+    issues.forEach((issue) => {
+        createTaskCard(issue);
+    });
+};   
+
+
